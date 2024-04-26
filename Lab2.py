6e849f27ff275bdaf50ca7db3c99eb491dd47aca
JJunJia <jjjunjia97.23@ichat.sp.edu.sg>
2024-04-26 14:50:22 +0800

    add code to check average and min and max

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,33 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+
+def main():
+  print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+  display_main_menu()
+  num_list = get_user_input()
+  average=calc_average(num_list)
+  min_max=calc_min_max_temperature(num_list)
+  print(average)
+  print("Minimum & Maximum temperature is ")
+  print(min_max)
+  
+
+def display_main_menu():
+    print("Enter some numbers separated by commas")
+
+def get_user_input():
+  numbers=input("Enter your numbers:")
+  x = [float(value) for value in numbers.split(", ")]
+  return x
+
+def calc_average(num_list):
+ print("calc_average")
+ countave=sum(num_list)/len(num_list)
+ return countave
+
+def calc_min_max_temperature(num_list):
+  countmin_max=[min(num_list),max(num_list)]
+  return countmin_max
+
+if __name__ == "__main__":
+ main()
+
+ 
