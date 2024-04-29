17ff007712108cec9aa39be00055e860e7401898
JJunJia <jjjunjia97.23@ichat.sp.edu.sg>
2024-04-29 14:48:27 +0800

    Added return code

--- bmi.py
+++ bmi.py
@@ -1,3 +1,6 @@
+def main():
+ print(calculate_bmi(weight=30, height=1.73))
+
 def calculate_bmi(height, weight):
  print("Height = " + str(height))
  print("Weight = " + str(weight))
@@ -5,8 +8,14 @@ def calculate_bmi(height, weight):
  print("BMI = " + str(bmi))
  if bmi<18.5:
   print("Under Weight")
+  return(-1)
  elif bmi>=18.5 and bmi<=25.0:
   print ("Normal Weight")
+  return (0)
  else:
   print ("Over Weight") 
-calculate_bmi(weight=57, height=1.73)
+  return (1)
+
+
+if __name__ == "__main__":
+ main()
